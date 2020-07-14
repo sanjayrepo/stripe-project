9f4d8c4b3a4d3cc185141d9f9675530ae78f7ab0
sanjay_pradhan <sanjay.p@nooor.in>
2020-07-14 20:54:44 +0530

    stripe customer creation with minimum details

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const cors = require('cors')
 const express = require('express')
-// const stripe = require("stripe")("")
+const stripe = require("stripe")("sk_test_51H4dDvEwwA97aKz6s7mwTMpA8ylxhSK4rYYFB2yPLkjEfeBG8hvGvpkIFv5fUoVjBq4IH9xNcLHgFlT0sEQgWCTi009Kmr2WaK")
 const uuid = require("uuid")
 const app = express();
 
@@ -11,6 +11,33 @@ app.use(cors())
 //routes
 app.get("/", (req, res) => {
     res.send("Sucessfully working")
+})
+
+app.post("/payment", (req, res) => {
+    const { product, token } = request.body;
+    console.log("product", product);
+    console.log("price", product.price);
+    const idempotencyKey = uuid()
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    }).then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'usd',
+            customer: customer.id,
+            reciept_email: token.email,
+            description: `purchase of ${product.name}`,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    country: token.card.address_country
+                }
+            }
+        }, { idempotencyKey })
+    })
+        .then(result => res.status(200).json(result))
+        .catch(err => console.log(err))
 })
 
 //listen
